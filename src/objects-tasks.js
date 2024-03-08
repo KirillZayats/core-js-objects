b8369b7fc9535a7ce466aaa706076cd09db294ad
unknown <kirill.zayats.99@mail.ru>
2024-03-08 14:27:29 +0300

    feat: add solutions for tasks (2/3)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -109,8 +109,8 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -129,8 +129,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -143,8 +143,19 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const word = [];
+  let length = 0;
+  Object.entries(lettersObject).forEach(([value]) => {
+    length += value.length;
+  });
+  word.length = length;
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((position) => {
+      word[position] = key;
+    });
+  });
+  return word.join('');
 }
 
 /**
@@ -161,8 +172,20 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let bank = 0;
+  const priceTicket = 25;
+  let isStatus = true;
+  queue.forEach((element) => {
+    if (element === priceTicket) {
+      bank += priceTicket;
+    } else if (element - bank <= priceTicket) {
+      bank += priceTicket;
+    } else {
+      isStatus = false;
+    }
+  });
+  return isStatus;
 }
 
 /**
@@ -178,8 +201,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
